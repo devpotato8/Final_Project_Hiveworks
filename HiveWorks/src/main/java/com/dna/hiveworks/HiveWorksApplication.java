ed5f88faf147319d2f85d4079f944b8a9a082ba9
ljydev6 <ljydev6@gmail.com>
2023-12-20 10:49:22 +0900

    23/12/20 이재연 - 프로젝트 기초 세팅

---
+++ HiveWorks/src/main/java/com/dna/hiveworks/HiveWorksApplication.java
@@ -0,0 +1,13 @@
+package com.dna.hiveworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiveWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiveWorksApplication.class, args);
+	}
+
+}
