ce69969473b81e2dd1f2bbc9f1c8992a5f26f340
user <user@host.docker.internal>
2023-12-28 16:45:53 +0900

    salary _ KTH 1. 급여관련 controller, service, dao 생성
    Merge branch 'dev' of https://github.com/ljydev6/Hiveworks into KTH

---
+++ HiveWorks/src/main/java/com/dna/hiveworks/controller/SalaryController.java
@@ -0,0 +1,40 @@
+package com.dna.hiveworks.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.dna.hiveworks.serviceimpl.SalaryServiceImpl;
+
+import lombok.AllArgsConstructor;
+
+
+/**
+ * @author : 김태하
+ * @since : 2023. 12. 28.
+ * Description : 급여정보 서비스 구현
+ * 
+ * History :
+ * - 작성자 : 김태하, 날짜 : 2023. 12. 28., 설명 : 최초작성
+ * 
+ */
+
+
+@AllArgsConstructor
+@RequestMapping("/salary")
+public class SalaryController {
+
+	
+	private final SalaryServiceImpl service;
+	
+	@RequestMapping("/salaryList.do")
+	public String selectSalaryListAll(Model model, @RequestParam(defaultValue="1") int cPage){
+		//List<Salary> list = service.selectSalaryListAll();
+		
+		//model.addAttribute(list);
+		
+		return "/salaryList";
+	}
+	
+	
+}
