ed2b1b4b96919bc60ffc9baba0962e9cfcddc399
wogml <wogml@host.docker.internal>
2024-01-16 20:28:09 +0900

    JHEEHO 파일 오류수정

---
+++ HiveWorks/src/main/java/com/dna/hiveworks/model/daoimpl/SalaryDaoImpl.java
@@ -0,0 +1,58 @@
+package com.dna.hiveworks.model.daoimpl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.dna.hiveworks.model.dao.SalaryDao;
+import com.dna.hiveworks.model.dto.salary.Salary;
+
+@Repository
+public class SalaryDaoImpl implements SalaryDao {
+
+	@Override
+	public List<Salary> selectSalaryListAll(SqlSession session) {
+		return session.selectList("salary.selectSalaryListAll");
+	}
+
+	@Override
+	public Salary selectSalaryByNo(SqlSession session, int sal_no) {
+		return session.selectOne("salary.selectSalaryByNo", sal_no);
+	}
+
+	@Override
+	public int insertSalary(SqlSession session, Salary s) {
+		return session.insert("salary.insertSalary",s);
+	}
+
+	@Override
+	public int updateSalary(SqlSession session, Salary s) {
+		return session.update("salary.updateSalary", s);
+	}
+
+	@Override
+	public int deleteSalary(SqlSession session, int sal_no) {
+		return session.delete("salary.deleteSalary",sal_no);
+	}
+
+	@Override
+	public int updatePositionPay(SqlSession session, Salary s) {
+		return session.update("salary.updatePositionPay",s);
+	}
+
+	@Override
+	public int insertPositionPay(SqlSession session, Salary s) {
+		
+		return session.insert("salary.insertPositionPay",s);
+	}
+
+	@Override
+	public Map<String, Integer> calculateSalary(SqlSession session, Map<String, Integer> data) {
+		return session.selectOne("salary.calculateSalary",data);
+	}
+
+
+}
---
+++ HiveWorks/src/main/java/com/dna/hiveworks/serviceimpl/SalaryServiceImpl.java
@@ -0,0 +1,70 @@
+package com.dna.hiveworks.serviceimpl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Service;
+
+import com.dna.hiveworks.model.daoimpl.SalaryDaoImpl;
+import com.dna.hiveworks.model.dto.salary.Salary;
+import com.dna.hiveworks.service.SalaryService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class SalaryServiceImpl implements SalaryService {
+
+	
+	private final SalaryDaoImpl dao;
+	private final SqlSession session;
+	
+	@Override
+	public List<Salary> selectSalaryListAll() {
+		return dao.selectSalaryListAll(session);
+	}
+
+	@Override
+	public Salary selectSalaryByNo(int sal_no) {
+		return dao.selectSalaryByNo(session, sal_no);
+	}
+
+	@Override
+	public int insertSalary(Salary s) {
+		
+		int result = dao.insertSalary(session, s);
+		if(result<=0) {
+			new RuntimeException("등록 실패");
+		}
+		
+		return result;
+	}
+
+	@Override
+	public int updateSalary(Salary s) {
+		
+		int result = dao.updateSalary(session, s);
+		if(result>0) {
+			
+		}else {
+			new RuntimeException("업데이트 실패");
+		}
+		
+		return result;
+	}
+
+	@Override
+	public int deleteSalary(int sal_No) {
+		return dao.deleteSalary(session, sal_No);
+	}
+
+	@Override
+	public Map<String, Integer> calculateSalary(Map<String,Integer> data) {
+		return dao.calculateSalary(session, data);
+	}
+	
+	
+
+	
+}
